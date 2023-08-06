9ce2bc4566e095fb33f43ff0ac9648640991cf8a
SergUs75 <131398452+SergUs75@users.noreply.github.com>
2023-08-06 22:29:21 +0300

    виправлення помилок. Додано функція пошуку по нататкам

--- notes_class.py
+++ notes_class.py
@@ -5,29 +5,26 @@ from collections import UserDict
 class Field:
 
     def __init__(self, value):
-        self.__value = None
         self.value = value
 
-    def __eq__(self, other):
+    """def __eq__(self, other):
         if isinstance(other, Field):
-            return self.value == other.value
+            return self.value == other.value"""
 
     def __str__(self):
-        return str(self.value)
+        return self.value
 
     def __repr__(self):
         return str(self)
 
 
 class Tag(Field):
-
-    def __str__(self):
-        return f"{self.value}"
+    ...
 
 
 class Note:
 
-    def __init__(self, name: Field, content: Field, tag: Tag = None):
+    def __init__(self, name: Field, content: Field, tag: Tag = None) -> None:
         self.name = name
         self.content = content
         self.tags = []
@@ -36,13 +33,17 @@ class Note:
         self.status = True
 
     def __str__(self):
-        return f"# '{self.name.value}': {self.content.value} (tegs: {', '.join(tag.value for tag in self.tags)})"
+        return f"'{self.name.value}' {self.content.value} (tegs: {', '.join(tag.value for tag in self.tags)}) статус: {str(self.status)} "
 
     def add_tag(self, tag):
         self.tags.append(tag)
 
 
 class Notebook(UserDict):
+    def __init__(self):
+        self.data = {}
+        self.tags_list = []
+        
     def add_note(self, note):
         self.data[note.name.value] = note
         return f"Note '{note.name}' added successfully."
@@ -52,21 +53,29 @@ class Notebook(UserDict):
 
     def __repr__(self):
         return str(self)
+    
+    
+    def search_in_notes(self, option, search: str):
+        result = []
+        for record in self.data.values():
+            if str(search).lower() in str(getattr(record, option)).lower():
+                result.append(record)
+        return result
 
     def save_json(self, file_path):
         json_data = {
-            "tags": list(tags_list),
-            "records": []
+            "tags": self.tags_list,
+            "notes": []
         }
 
         for note in self.data.values():
-            json_record = {
+            json_note = {
                 "name": note.name.value,
                 "content": note.content.value,
                 "tags": [tag.value for tag in note.tags],
                 "status": note.status
             }
-            json_data["records"].append(json_record)
+            json_data["notes"].append(json_note)
 
         with open(file_path, 'w', encoding='utf-8') as file:
             json.dump(json_data, file, indent=2, ensure_ascii=False)
@@ -75,7 +84,7 @@ class Notebook(UserDict):
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
-                for record in data["records"]:
+                for record in data["notes"]:
                     name = Field(record["name"])
                     content = Field(record["content"])
                     tags = [Tag(tag) for tag in record["tags"]]
@@ -83,8 +92,6 @@ class Notebook(UserDict):
                     for tag in tags:
                         note.add_tag(tag)
                     self.data[name.value] = note
-                global tags_list
-                tags_list = data["tags"]
-        except (FileNotFoundError, json.JSONDecodeError):
+                self.tags_list = data["tags"]
+        except (FileNotFoundError):
             print(f"The file {file_path} is missing or does not contain valid JSON data.")
-            return None
--- notes_main.py
+++ notes_main.py
@@ -1,13 +1,98 @@
-from notes_class import Notebook
+from notes_class import Notebook, Note
 
 
 note_book = Notebook()
 
+OPTIONS_SEARCH = {
+        1: ["name", "text"],
+        2: ["content", "text"],
+        3: ["tags", "text"],  
+        4: ["status", "true/false"],
+        5: []
+    }
+
+def get_input_int():
+    try:
+        user_input = int(input("Виберіть номер: "))
+        return user_input
+    except ValueError:
+        print("!!! Помилка: Введіть число:")
+        return get_input_int()
+
+def edit_note(record):
+    print(f"Знайдено запис: \n{record}"
+            "\n.............."
+            "\nМеню запису:"
+            "\n1. видалити"
+            "\n2. редагувати"
+            "\n3. змінити статус"
+            "\n4. головне меню")
+    option = get_input_int()
+    
+    if option == 1:
+        pass   
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        record = None
+        return
+    else:
+        print("Некоректний вибір. Спробуйте ще раз.")
+
+
+
+def search_note(option):
+    try:
+        search_params = OPTIONS_SEARCH[option]
+    except KeyError:
+        print("!!! Помилка: Невірний вибір параметра пошуку")
+        return
+
+    if not search_params:
+        return
+
+    search_term = input(f"Введіть значення для пошуку в {search_params[0]} (format input: {search_params[1]}): ")
+    
+    result = (note_book.search_in_notes(search_params[0], search_term))
+    
+    if len(result) == 1:
+        return edit_note(result[0])
+    
+    elif len(result) > 0:
+        dict_results = {i+1: record for i, record in enumerate(result)}
+        for id, record in dict_results.items():
+            print(id, record)
+        option = get_input_int()       
+        if option in dict_results: 
+            selected = dict_results[option]
+            return edit_note(selected)
+    else:
+        print("Результатів пошуку немає")
+        return search_menu()
+
+
+def search_menu():
+    while True:
+        print(".............."
+            "\nПошук в нотатках:"
+            "\n1. за назвою"
+            "\n2. за вмістом"
+            "\n3. за тегом"
+            "\n4. за статусом"
+            "\n5. повернутися на головне меню")
+    
+        option = get_input_int()  
+        search_note(option)
+        if option in OPTIONS_SEARCH:
+            break
+
 
 def show_comand():
     print("Всі нотатки:")
-    print(note_book)
-    input("повернутися в Меню (натиснути Enter)")
+    print(note_book, '\n')
+    #input("повернутися в Меню (натиснути Enter)")
 
 
 def main():
@@ -15,27 +100,25 @@ def main():
     print("Notebook дані завантажено.")
 
     while True:
-        print("Меню:"
-            "\n1. Створити новий нотаток"
-            "\n2. Показати всі нотатки"
-            "\n3. Пошук нотатку за тегом"
-            "\n4. Видалити нотатку"
-            "\n5. Вийти")
-        user_input = int(input("Виберіть опцію: "))
+        print(".............."
+            "\nМеню нотатків:"
+            "\n1. показати всі"
+            "\n2. створити"
+            "\n3. пошук"
+            "\n4. вийти")
+        
+        user_input = get_input_int()
 
         if user_input == 1:
-            pass
-
-        elif user_input == 2:
             show_comand()
 
-        elif user_input == 3:
+        elif user_input == 2:
             pass
+
+        elif user_input == 3:
+            search_menu()
 
         elif user_input == 4:
-            pass
-
-        elif user_input == 5:
             note_book.save_json('notebook.json')
             print("Notebook дані збережено.")
             break
