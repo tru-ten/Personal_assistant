1fea4cdac2ee09d7d1f9708edd64dee714fadd3e
SergUs75 <131398452+SergUs75@users.noreply.github.com>
2023-08-08 20:13:24 +0300

    Додано створення, редагівання, видалення нотатків, тегів, сортування

--- notes_class.py
+++ notes_class.py
@@ -7,15 +7,21 @@ class Field:
     def __init__(self, value):
         self.value = value
 
-    """def __eq__(self, other):
-        if isinstance(other, Field):
-            return self.value == other.value"""
-
     def __str__(self):
         return self.value
 
     def __repr__(self):
         return str(self)
+    
+    def __eq__(self, other):
+        if isinstance(other, Field):
+            return self.value == other.value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __gt__(self, other):
+        return self.value > other.value
 
 
 class Tag(Field):
@@ -33,35 +39,85 @@ class Note:
         self.status = True
 
     def __str__(self):
-        return f"'{self.name.value}' {self.content.value} (tegs: {', '.join(tag.value for tag in self.tags)}) статус: {str(self.status)} "
-
-    def add_tag(self, tag):
-        self.tags.append(tag)
-
+        return f"'{self.name.value}' {self.content.value} (tegs: {', '.join(tag.value for tag in self.tags)}) status: {str(self.status)} "
+    
+    def add_tag_in_tags(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+        else:
+            print(f"Tag {tag} already exists")
+    
+    def remove_tag(self, tag):
+        self.tags.remove(tag)
 
 class Notebook(UserDict):
     def __init__(self):
         self.data = {}
         self.tags_list = []
-        
-    def add_note(self, note):
-        self.data[note.name.value] = note
-        return f"Note '{note.name}' added successfully."
-
+    
     def __str__(self):
         return "\n".join(str(note) for note in self.data.values())
 
     def __repr__(self):
         return str(self)
     
+    """def __eq__(self, other):
+        if isinstance(other, Field):
+            return self.value == other.value"""
+        
+    def add_note(self, note):
+        self.data[note.name.value] = note
+        return f"Note '{note}' added successfully."
     
-    def search_in_notes(self, option, search: str):
+    def search_in_notes(self, field, search: str):
         result = []
-        for record in self.data.values():
-            if str(search).lower() in str(getattr(record, option)).lower():
-                result.append(record)
+        for note in self.data.values():
+            if str(search).lower() in str(getattr(note, field)).lower():
+                result.append(note)
         return result
+    
+    def delete_in_note(self, note):
+        del self.data[note.name.value]
+        print("Record deleted")
+    
+    def edit_in_note(self, note, field, new_value):
+        setattr(note, field, new_value)
+        print(f"Note updated: {note}")
 
+    def edit_status_in_note(self, note):
+        setattr(note, "status", False) 
+        print(f"Note status updated: {note}")
+
+    def sort_by_tags(self):
+        notes = list(self.data.values())
+        notes.sort(key=lambda note: note.tags)
+        return notes
+    
+    def add_tag_note(self, new_tag):
+        if new_tag.value not in self.tags_list:
+            self.tags_list.append(new_tag.value)
+            print(f"Тег {new_tag.value} додано")
+        else:
+            print(f"Тег {new_tag.value} вже існує")
+    
+    def search_in_tags(self, search):
+        tags = []
+        for tag in self.tags_list:
+            if search.lower() in tag.lower():
+                tags.append(tag)
+        return tags
+    
+    def get_tag_values(self):
+        return self.tags_list
+
+    def add_tag_note(self, new_tag):
+        if new_tag.value not in self.tags_list:
+            self.tags_list.append(new_tag.value)
+            print(f"Тег {new_tag.value} додано")
+        else:
+            print(f"Тег {new_tag.value} вже існує")
+    
+    
     def save_json(self, file_path):
         json_data = {
             "tags": self.tags_list,
@@ -90,7 +146,7 @@ class Notebook(UserDict):
                     tags = [Tag(tag) for tag in record["tags"]]
                     note = Note(name, content)
                     for tag in tags:
-                        note.add_tag(tag)
+                        note.add_tag_in_tags(tag)
                     self.data[name.value] = note
                 self.tags_list = data["tags"]
         except (FileNotFoundError):
--- notes_main.py
+++ notes_main.py
@@ -1,12 +1,11 @@
-from notes_class import Notebook, Note
-
+from notes_class import Notebook, Note, Field, Tag
 
 note_book = Notebook()
 
-OPTIONS_SEARCH = {
-        1: ["name", "text"],
-        2: ["content", "text"],
-        3: ["tags", "text"],  
+OPTIONS = {
+        1: ["name", "text", Field],
+        2: ["content", "text", Field],
+        3: ["tags", "text", list],  
         4: ["status", "true/false"],
         5: []
     }
@@ -16,11 +15,25 @@ def get_input_int():
         user_input = int(input("Виберіть номер: "))
         return user_input
     except ValueError:
-        print("!!! Помилка: Введіть число:")
-        return get_input_int()
+        print("Помилка. Введіть число.")  
+    except KeyError:
+        print("Некоректний вибір. Спробуйте ще раз.")
+    return get_input_int()
 
-def edit_note(record):
-    print(f"Знайдено запис: \n{record}"
+def numerator(list_result):
+    if len(list_result) > 0:
+        dict_results = {i+1: record for i, record in enumerate(list_result)}
+        for id, record in dict_results.items():
+            print(id, record)
+        option_input = get_input_int()
+        if option_input in dict_results:
+            result = dict_results[option_input]
+            return result
+    elif len(list_result) == 0:
+        return None
+
+def content_note(note):
+    print(f"Знайдено запис: \n{note}"
             "\n.............."
             "\nМеню запису:"
             "\n1. видалити"
@@ -28,72 +41,136 @@ def edit_note(record):
             "\n3. змінити статус"
             "\n4. головне меню")
     option = get_input_int()
-    
     if option == 1:
-        pass   
+        return note_book.delete_in_note(note) 
     elif option == 2:
-        pass
+        return note_menu("Редактор нотатків:", edit_note, note)
     elif option == 3:
-        pass
+        return note_book.edit_status_in_note(note) 
     elif option == 4:
-        record = None
         return
+
+def search_tags(note):
+    search = input(f"Введіть значення для поля tags (format input: text): ")
+    tag_list = note_book.search_in_tags(search)
+    tag = numerator(tag_list)
+    if tag:  
+        print(f"знайдено тег {tag}")
+        return tag
     else:
-        print("Некоректний вибір. Спробуйте ще раз.")
+        print("Результатів пошуку немає"
+              "\n1 додати новий тег"
+              "\n2 повернутися в меню")
+        option = get_input_int()
+        if option == 1:
+            add_tag_in_note(search, note)
+        elif option == 2:
+            return edit_tag(note)
 
+def add_tag_in_note(tag_name, note):
+    new_tag = Tag(tag_name)
+    note.add_tag_in_tags(new_tag)
+    note_book.add_tag_note(new_tag)
+    return edit_tag(note)
 
+def search_tag_in_tegs():
+    list_tags = note_book.get_tag_values()
+    tag = numerator(list_tags)
+    return tag
 
-def search_note(option):
+def edit_tag(note):
+    while True:
+        print(f"Запис: {note}"
+            "\n............."
+            "\nменю тег"
+            "\n1 видалити"
+            "\n2 додати"
+            "\n3 повернуимсь в меню запису")
+        option = get_input_int()
+        if option == 1:
+            tag_search = search_tag_in_tegs()
+            print(tag_search)
+            if tag_search:
+                tag = Tag(tag_search)
+                note.remove_tag(tag)
+        elif option == 2:  
+            tag_search = search_tags(note)
+            tag = Tag(tag_search)    
+            note.add_tag_in_tags(tag)     
+        elif option == 3:
+            return content_note(note)
+
+def edit_note(option, note):
     try:
-        search_params = OPTIONS_SEARCH[option]
+        search_params = OPTIONS[option]
+    except KeyError:
+        return print("!!! Помилка: Невірний вибір параметра пошуку")    
+    if not search_params:
+        return print("!!! Помилка: Невірний вибір параметра пошуку")  
+    if search_params[0] == "tags":
+        return edit_tag(note)
+    if search_params[0] == "status":
+        return note_book.edit_status_in_note(note)  
+    input_content = input(f"Введіть значення для пошуку в {search_params[0]} (format input: {search_params[1]}): ")
+    new_value = Field(input_content)
+    note_book.edit_in_note(note, search_params[0], new_value)
+
+def search_note(option, note):
+    try:
+        search_params = OPTIONS[option]
     except KeyError:
         print("!!! Помилка: Невірний вибір параметра пошуку")
         return
-
     if not search_params:
         return
-
-    search_term = input(f"Введіть значення для пошуку в {search_params[0]} (format input: {search_params[1]}): ")
-    
-    result = (note_book.search_in_notes(search_params[0], search_term))
-    
-    if len(result) == 1:
-        return edit_note(result[0])
-    
-    elif len(result) > 0:
-        dict_results = {i+1: record for i, record in enumerate(result)}
-        for id, record in dict_results.items():
-            print(id, record)
-        option = get_input_int()       
-        if option in dict_results: 
-            selected = dict_results[option]
-            return edit_note(selected)
+    search_term = input(f"Введіть значення для поля {search_params[0]} (format input: {search_params[1]}): ")
+    list_result = note_book.search_in_notes(search_params[0], search_term)
+    result = numerator(list_result)
+    if result:
+        content_note(result)
     else:
         print("Результатів пошуку немає")
-        return search_menu()
+        return note_menu("Пошук в нотатках:", search_note)
 
-
-def search_menu():
+def note_menu(name, func, note=None):
     while True:
         print(".............."
-            "\nПошук в нотатках:"
+            f"\n{name}"
             "\n1. за назвою"
             "\n2. за вмістом"
             "\n3. за тегом"
             "\n4. за статусом"
             "\n5. повернутися на головне меню")
-    
         option = get_input_int()  
-        search_note(option)
-        if option in OPTIONS_SEARCH:
+        return func(option, note)
+    
+def add_note():
+    print("Створення нотатка")
+    name = input("Введіть назву нотатки: ")
+    name = Field(name)
+    content = input("Введіть вміст нотатки: ") 
+    content = Field(content)
+    note = Note(name, content)
+    note_book.add_note(note)
+    while True:
+        print("1 додати тег\n2 продовжити")
+        option = get_input_int()
+        if option == 1:
+            tag = search_tag_in_tegs()
+            tag = Tag(tag)
+            if tag not in note.tags:
+                note.add_tag_in_tags(tag)
+                print(f"До запису {name} додано новий тег {tag}")  
+            else:
+                print(f"Тег {tag} вже існує")
+        elif option == 2:
             break
-
+    print(note_book.add_note(note))
 
 def show_comand():
     print("Всі нотатки:")
     print(note_book, '\n')
-    #input("повернутися в Меню (натиснути Enter)")
-
+    input("повернутися в Меню (натиснути Enter)")
 
 def main():
     note_book.load_json('notebook.json')
@@ -102,10 +179,11 @@ def main():
     while True:
         print(".............."
             "\nМеню нотатків:"
-            "\n1. показати всі"
-            "\n2. створити"
-            "\n3. пошук"
-            "\n4. вийти")
+            "\n1 показати всі"
+            "\n2 створити"
+            "\n3 пошук"
+            "\n4 сортування за тегами"
+            "\n5 вийти")
         
         user_input = get_input_int()
 
@@ -113,12 +191,17 @@ def main():
             show_comand()
 
         elif user_input == 2:
-            pass
+            add_note()
 
         elif user_input == 3:
-            search_menu()
+            note_menu("Пошук в нотатках:", search_note)
 
         elif user_input == 4:
+            sorted_notes = note_book.sort_by_tags()
+            for note in sorted_notes:
+                print(note)
+
+        elif user_input == 5:
             note_book.save_json('notebook.json')
             print("Notebook дані збережено.")
             break
