7ec7df992bc40e4fc7aeb1db33e34c6ce918bebb
tru-ten <74614544+tru-ten@users.noreply.github.com>
2023-08-10 18:01:35 +0300

    Creating setup

---
+++ triton/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='triton',
+      version='1.0',
+      description='Personal assistant to manage notes, contacts and files',
+      url='https://github.com/tru-ten/Personal_assistant',
+      author='team_6th.py',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points = {'console_scripts': 'starttriton = triton.triton:main'}
+      )
